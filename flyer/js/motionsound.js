84b9cb267cbe198d96d426442827ee8abfb5e8fa
Sam Decrock <sam.decrock@gmail.com>
2014-02-14 15:09:57 +0100

    left & right volume zit er in

--- flyer/js/motionsound.js
+++ flyer/js/motionsound.js
@@ -9,7 +9,25 @@ var Motionsound = function (options){
 	};
 
 	var onDeviceorientation = function (event) {
-		konsole.log( Math.round(event.gamma)+" | "+ Math.round(event.beta)+" | "+ Math.round(event.alpha));
+		// konsole.log( Math.round(event.gamma)+" | "+ Math.round(event.beta)+" | "+ Math.round(event.alpha));
+
+		// konsole.log( Math.round(event.gamma) );
+
+		var degrees = event.gamma;
+
+		// limit to -90 and 90 (so it's the same on Android and iOS)
+		if(degrees > 90)
+			degrees = 90;
+
+		if(degrees < -90){
+			degrees = -90;
+		}
+
+		if( -90 < degrees && degrees < 0){
+			if(options.onLeftVolumeChanged) options.onLeftVolumeChanged( Math.abs(degrees)/90 );
+		}else if( 0 < degrees && degrees < 90){
+			if(options.onRightVolumeChanged) options.onRightVolumeChanged( Math.abs(degrees)/90 );
+		}
 	};
 
 	return {
@@ -18,9 +36,24 @@ var Motionsound = function (options){
 };
 
 
+// knippen en plakken in de main.js en gebruiken waar nodig:
 
 $(function(){
-	var motionsound = new Motionsound();
-	// motionsound.init();
+	var motionsound = new Motionsound({
+		onLeftVolumeChanged: leftVolumeChanged,
+		onRightVolumeChanged: rightVolumeChanged
+	});
+	motionsound.init();
 });
 
+
+function leftVolumeChanged(value){
+	// value is een waarde tussen 0 en 1
+	konsole.log( 'left volume: ' + Math.round(value*100)/100 );
+}
+
+
+function rightVolumeChanged(value){
+	// value is een waarde tussen 0 en 1
+	konsole.log( 'right volume: ' + Math.round(value*100)/100 );
+}
