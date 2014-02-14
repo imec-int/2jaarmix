c5e746a8147f09aaba23be1d7aab6ccb197e4a19
Robby Wauters <robbywauters@gmail.com>
2014-02-14 13:43:23 +0100

    iOS layout probs fix

--- flyer/js/main.js
+++ flyer/js/main.js
@@ -38,7 +38,6 @@ function setClickHandlers () {
 	$(".title").click(function(event){
 		event.preventDefault();
 		$(".title").hide();
-		$(".body").css("-webkit-filter", "hue-rotate(50deg)");
 
 		return false;
 	});
@@ -55,7 +54,8 @@ function updateRightArray(){
 		rightArray[i] = $(el).hasClass("sequence-right");
 	})
 }
-var colors = ["#8DBDC7","#FFE37F","#F0C271","#E3A368","#D18461"];
+// var colors = ["#8DBDC7","#FFE37F","#F0C271","#E3A368","#D18461"];
+var colors = ["#0A87BF","#03657F","#00F2FF","#011940","#0781E5", "#BF09A1","#13207F","#E098FF","#120040","#A841E5"];
 var titleArray1 = ["LET'S","MAKE","SOME","MUSIC"];
 var titleArray2 = ["HACK","THIS","PRESS","HERE"];
 var hasStarted = false;
@@ -95,10 +95,10 @@ function sequenceTick(){
 		currentSequence ++;
 	}
 	var top = parseInt($("#flow-sequencer").css("top"), 10);
-	// console.log("sequenceTick - "+ top);
+	// console.log("rgb("+(Math.floor(Math.random()*128))+","+(Math.floor(Math.random()*64))+","+(Math.floor(Math.random()*255))+")");
 	$("#flow-sequencer").css("top",currentSequence*70);
-
-	$("body").css("background-color",colors[currentSequence]);
+	// $("body").css("background-color",colors[Math.floor(Math.random()*colors.length)]);
+	$("body").css("background-color","rgb("+(Math.floor(Math.random()*128))+","+(Math.floor(Math.random()*64))+","+(Math.floor(Math.random()*255))+")");
 	if(!hasStarted){
 		$(".title > h2").text(titleArray1[currentSequence]);
 	}else{
