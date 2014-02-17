ef01119966fa98091f6aedfd8b93966de517fe5e
Sam Decrock <sam.decrock@gmail.com>
2014-02-17 14:18:38 +0100

    changed text to "Hack this music now!"

--- flyer/js/main.js
+++ flyer/js/main.js
@@ -1,6 +1,6 @@
 // var colors = ["#8DBDC7","#FFE37F","#F0C271","#E3A368","#D18461"];
 var colors = ["#0A87BF","#03657F","#00F2FF","#011940","#0781E5", "#BF09A1","#13207F","#E098FF","#120040","#A841E5"];
-var titleArray1 = ["LET'S","HACK","SOME","MUSIC"];
+var titleArray1 = ["HACK","THIS","MUSIC","NOW!"];
 var titleArray2 = ["MEDIA HACKATHON","Zat. 15/03","AB BRUSSEL","-> Inschrijven! <-"];
 var hasStarted = false;
 var interval = 0;
